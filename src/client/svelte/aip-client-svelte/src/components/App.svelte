3fdc9c19aa024938dfc968eef6ebed36a3a996cc
StefanHajdu <stevo.hajdu006@gmail.com>
2024-10-11 22:04:45 +0200

    svelte for aip

---
+++ src/client/svelte/aip-client-svelte/src/components/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import TailwindTryOut from "./TailwindTryOut.svelte";
+  import FlowbiteTryOut from "./FlowbiteTryOut.svelte";
+</script>
+
+<TailwindTryOut />
+<FlowbiteTryOut />
